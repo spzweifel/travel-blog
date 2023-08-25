27bdbbec22ebcd95cf7901ddcb71a803002cb81a
Sean Zweifel <spzweifel@gmail.com>
2023-08-24 21:26:29 -0400

    the server worksgit add -A!

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+// import { Link } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_MATCHUPS } from '../utils/queries';
+
+const Home = () => {
+    // const { loading, data } = useQuery(QUERY_MATCHUPS, {
+    //   fetchPolicy: "no-cache"
+    // });
+  
+    // const matchupList = data?.matchups || [];
+    return (
+        <div className="home-page">
+            <h1>This is my travel blog</h1>
+            <h2>Let's find our next adventure!</h2>
+       </div>
+    );
+}
+
+export default Home;
--- server/server.js
+++ server/server.js
@@ -1,34 +1,43 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const { authMiddleware } = require("./utils/auth.js");
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-const app = express();
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve static assets (React build files) if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/src")));
-
-  // Serve React app's HTML file for the root URL
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/src/Home.js"));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-
-// // Example route
-// app.get("/api/Home", (req, res) => {
-//   res.json({ message: "This is an example route." });
-// });
-
-// Start server
-db.once("open", () => {
-  app.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}/`)
-  );
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer();
+ 
+
